8ee4aab947730a477874bd84cdd08328d82b2519
Jean Carlo <jeansanto2002@gmail.com>
2024-02-12 11:42:58 -0300

    Criando teste de unidade

---
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -0,0 +1,70 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.domain.Abrigo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AbrigoServiceTest {
+
+    private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
+    private AbrigoService abrigoService = new AbrigoService(client);
+    private HttpResponse<String> response = mock(HttpResponse.class);
+    private Abrigo  abrigo = new Abrigo("Teste", "123456789", "teste@email.com");
+
+    @Test
+    public void listarAbrigo() throws IOException, InterruptedException {
+        String uri = "http://localhost:8080/abrigos";
+
+        when(client.dispararRequisicaoGet(uri)).thenReturn(response);
+
+        when(response.body()).thenReturn("[{\"id\":1,\"nome\":\"Abrigo 1\"},{\"id\":2,\"nome\":\"Abrigo 2\"}]");
+
+        HttpResponse<String> response = client.dispararRequisicaoGet(uri);
+        String responseBody = response.body();
+        Abrigo[] abrigos = new ObjectMapper().readValue(responseBody, Abrigo[].class);
+        List<Abrigo> abrigoList = Arrays.asList(abrigos);
+
+        System.out.println("Abrigos cadastrados:");
+        for (Abrigo abrigo : abrigoList) {
+            long id = abrigo.getId();
+            String nome = abrigo.getNome();
+            System.out.println(id + " - " + nome);
+        }
+    }
+
+    @Test
+    public void deveVerificarSeDispararRequisicaoGetSeraChamado() throws IOException, InterruptedException {
+        abrigo.setId(0L);
+        String expectedAbrigosCadastrados = "Abrigos cadastrados:";
+        String expectedIdENome = "0 - Teste";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[{"+abrigo.toString()+"}]");
+        when(client.dispararRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigos();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actualAbrigosCadastrados = lines[0];
+        String actualIdENome = lines[1];
+
+        Assertions.assertEquals(expectedAbrigosCadastrados, actualAbrigosCadastrados);
+        Assertions.assertEquals(expectedIdENome, actualIdENome);
+    }
+}
